673bddd4fc312e2ebf046c6a2cfa5dc662626578
Ray Zhang <87215745+Raysekiro@users.noreply.github.com>
2024-06-25 13:42:41 +0800

    update

--- initializeDEMandH.m
+++ initializeDEMandH.m
@@ -1,17 +1,17 @@
-function [xq, yq, vq, Dxq, Dyq, xq_q, yq_q, vq_q, Dxq_q, Dyq_q, slope, DepthDATA_M, ...
-    x_area_min, x_area_max, y_area_min, y_area_max, PeakScope, Depth, Z, AdjustX, AdjustY] ...
-    = initializeDEMandH(width, quiverWidth, gausscore, BATHYFile, flag1, flag2)
+function [xq, yq, vq, Dxq, Dyq, xq_q, yq_q, vq_q, Dxq_q, Dyq_q, slopeLowF, DepthDATA_M, ...
+    x_area_min, x_area_max, y_area_min, y_area_max, PeakScope, Depth, Z, ErosionDepth, AdjustX, AdjustY, AdjustZ] ...
+    = initializeDEMandH(width, quiverWidth, gausscore, BATHYFile, ScarShapedata, OrigindataResolution, flag1, flag2)
     % Define constants and ranges
    
-    if flag1 == 1 %Load peak data
+    if flag1 %Load peak data
         x_area_min = 0;
         x_area_max = 500;
         y_area_min = 0;
         y_area_max = 500;
-        Depth = 6;
+        Depth = 8;
         PeakScope = min(x_area_max, y_area_max) *4 / 5; 
         PeakSize = 300; % Resolution
-        PeakSizeScalar = 6;
+        PeakSizeScalar = 3;
         x = linspace(-4, 4, PeakSize);
         y = linspace(-4, 4, PeakSize);
         [X_0,Y_0] = meshgrid(x,y);
@@ -27,27 +27,45 @@ function [xq, yq, vq, Dxq, Dyq, xq_q, yq_q, vq_q, Dxq_q, Dyq_q, slope, DepthDATA
         AdjustY = []; % Return empty
     else
         % Load DEM data
-        [X, Y, Z, Z_Region, x_area_min, x_area_max, y_area_min, y_area_max, AdjustX, AdjustY] ...
-            = loadAndProcessTerrainData(gausscore, BATHYFile, flag2);
+        [X, Y, Z, ErosionDepth, Z_Region, x_area_min, x_area_max, y_area_min, y_area_max, AdjustX, AdjustY, AdjustZ] ...
+            = loadAndProcessTerrainData(gausscore,  BATHYFile, ScarShapedata, OrigindataResolution, flag2);
         PeakScope = 0; % Return empty
         DepthDATA_M = []; % Return empty
         Depth = 0; % Return empty
-        if strcmpi(flag2, 'Zinnen')
-            % Zinnen Sliding area
-            x_area_min = 1000;
-            x_area_max = 3500;
-            y_area_min = 180;
-            y_area_max = 1800;
-        end
+        
+        % Define a structure array for sliding areas
+        sliding_areas = struct(...
+            'Tri', struct('x_min', x_area_min, 'x_max', x_area_max, 'y_min', y_area_min, 'y_max', y_area_max), ...
+            'Zinnen', struct('x_min', 1000, 'x_max', 3500, 'y_min', 180, 'y_max', 1800), ...
+            'BD31_1_1d', struct('x_min', 2800, 'x_max', 4000, 'y_min', 3000, 'y_max', 4200), ...
+            'BD31_1_1d_detailed', struct('x_min', 3100, 'x_max', 3600, 'y_min', 3250, 'y_max', 3750), ...
+            'SCSPRMC14', struct('x_min', 400, 'x_max', 2200, 'y_min', 2000, 'y_max', 4000), ...
+            'SWIberiaNGAScar461', struct('x_min', 1700, 'x_max', 55700, 'y_min', 2500, 'y_max', 52500), ...
+            'SWIberiaPeak1', struct('x_min', 400, 'x_max', 46800, 'y_min', 1500, 'y_max', 36500) ...
+            );
+        area_selected = sliding_areas.SWIberiaPeak1;
+        
+        x_area_min = area_selected.x_min;
+        x_area_max = area_selected.x_max;
+        y_area_min = area_selected.y_min;
+        y_area_max = area_selected.y_max;
+
+        
+
     end
+    
     % Create query grids for interpolation and calculation
     [xq, yq] = meshgrid(x_area_min:width:x_area_max, y_area_min:width:y_area_max);
     vq = griddata(X, Y, Z, xq, yq);
-    if flag1 == 1
+    if flag1 
+        ErosionDepth = zeros(size(vq));
         vq(isnan(vq)) = 0;
+    else
+        ErosionDepth = griddata(X, Y, ErosionDepth, xq, yq);   
     end
+
     % Generate depth data
-    DepthDATA_M = Depth - ((Depth * (1 - 1 / 6)) * (vq - min(vq(:))) / (max(vq(:)) - min(vq(:))));      
+    % DepthDATA_M = Depth - ((Depth * (1 - 1)) * (vq - min(vq(:))) / (max(vq(:)) - min(vq(:))));      
     % Compute gradients for flow direction arrows
     [Dxq, Dyq] = gradient(vq, width);
     
@@ -56,11 +74,9 @@ function [xq, yq, vq, Dxq, Dyq, xq_q, yq_q, vq_q, Dxq_q, Dyq_q, slope, DepthDATA
     vq_q = griddata(X, Y, Z, xq_q, yq_q);
     [Dxq_q, Dyq_q] = gradient(vq_q, quiverWidth);
     
-    % Calculate slope in degrees
-    slope = slope2(xq, yq, vq, 'degrees');
-    % Note: Assuming slope2 is a custom function that you have available.
+    vq_smooth = imgaussfilt(vq, 50);
 
-    %  % Create depth data uniformly set to a specific value
-    % DepthDATA = Depth * ones(size(vq));
-    % DepthDATA_M = DepthDATA;  % assuming you might modify or mask it later
+    slopeLowF = slope2(xq, yq, vq_smooth, 'degrees');
+    DepthDATA = Depth * ones(size(vq));
+    DepthDATA_M = DepthDATA;  % assuming you might modify or mask it later
 end
--- setDynamicColormap.m
+++ setDynamicColormap.m
@@ -1,17 +1,26 @@
-function cmapType = setDynamicColormap(xq, yq, vq, data, AdjustclimMin, AdjustclimMax, pivotValue, flag)
+function cmapType = setDynamicColormap(xq, yq, vq, AdjustclimMin, AdjustclimMax, pivotValue, flag)
     % Create a figure, plot, and then close to not display
     fColor = figure('units', 'normalized', 'outerposition', [0 0 1 1], 'Visible', 'off');
-    surf(xq, yq, vq, 'CData', data, 'FaceColor', 'interp', 'EdgeColor', 'none');
+    surf(xq, yq, vq, 'FaceColor', 'interp', 'EdgeColor', 'none');
     clim([AdjustclimMin, AdjustclimMax]); % Set the color limits
+    
+    % Check the flag to determine the colormap
     if flag == 1
-    colormap(flipud(slanCM('RdBu')));
-    setPivot(gca, pivotValue);
-    cmapType = colormap(gca);  
-    % cmapType = cmocean('balance', 'pivot', pivotValue); % Apply cmocean colormap   
-    else
-    cmapType = colormap(parula);
+        cmapOrig = flipud(slanCM('RdBu')); % Get the original colormap
+        % Apply a pivot adjustment if necessary
+        setPivot(gca, pivotValue);
+    elseif flag == 2
+        cmapOrig = colormap(parula);
+    elseif flag == 3
+        cmapOrig = colormap(turbo);
     end
-    colormap(fColor, cmapType); % Apply the colormap to the figure
-    colorbar;
+    
+    % Rescale the colormap to have 256 colors
+    nColors = size(cmapOrig, 1);
+    cmapType = interp1(linspace(1, nColors, 256), cmapOrig, 1:256); % Rescale colormap
+    
+    colormap(fColor, cmapType); % Apply the rescaled colormap to the figure
+    colorbar; % Display a colorbar
     close(fColor); % Close the figure to prevent it from displaying
 end
+
